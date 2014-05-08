0b81e5ae9c7b9b56d76062eaf9555f5a143ea64c
N Venkatesh Naik <nvnvenki@gmail.com>
2014-05-08 19:41:26 +0530

    Removed some unused code and seperated a method to extract emotion

--- src/main/sentitude/feature_extraction/featureExtractor.py
+++ src/main/sentitude/feature_extraction/featureExtractor.py
@@ -25,7 +25,7 @@ class FeatureExtractor:
         normalizedSignal = self.mfcc.normalisation(preEmphasizedSignal)
         framedSignal = self.mfcc.framing(normalizedSignal,(self.rate*self.window_length),(self.rate*self.window_step))
         powerSpectrum = self.mfcc.power_spectrum(framedSignal,self.nfft)
-        filterBanks = self.mfcc.calculate_mel_filter_banks(num_filters = 28,nfft=self.nfft,sample_rate = self.rate)
+        filterBanks = self.mfcc.calculate_mel_filter_banks(num_filters=28,nfft=self.nfft,sample_rate = self.rate)
         (self.feature_vectors,self.energy) = self.mfcc.filter_bank_energies(filterBanks,powerSpectrum)
         self.logFilterBankEnergies = self.mfcc.log_filter_bank_energies(self.feature_vectors)
         self.mfcc_coeff_vectors = self.mfcc.lifter(self.mfcc.discrete_cosine_transforms(self.logFilterBankEnergies,self.num_cepstrals))
@@ -36,26 +36,14 @@ class FeatureExtractor:
         def append_delta(coeff,delta,delta_delta):
             rows = coeff.shape[0]
             for i in xrange(0,rows):
-                # final_feature_vectors.append(numpy.concatenate((coeff[i],delta[i],delta_delta[i])))
                 final_feature_vectors.append(numpy.concatenate((coeff[i], delta[i])))
 
             return numpy.array(final_feature_vectors)
         final_features = append_delta(self.mfcc_coeff_vectors,self.delta_vectors,self.delta_delta_vectors)
-
-        # final_features = self.delta_vectors
-        # final_features = self.mfcc_coeff_vectors
-        ## Scikit begins ##
-        # from scikits.talkbox.features import mfcc
-        # final_features, mspec, spec = mfcc(self.signal)
-        # final_features = self.mfcc.delta_coefficients(final_features, 1)
-        ## Scikit ends ##
-
         final_features = final_features[~numpy.isnan(final_features).any(axis = 1)]
         final_features = final_features[~numpy.isinf(final_features).any(axis = 1)]
         final_features = final_features[~numpy.isneginf(final_features).any(axis = 1)]
-
         return final_features
-
 
 def main():
     extractor = FeatureExtractor("/home/venkatesh/Venki/FINAL_SEM/Project/Datasets/Happiness/HappinessAudios/1.wav")
