4225b753d22454a46c761e3026169b5b2a174151
nvnvenki <nvnvenki@gmail.com>
2014-05-26 21:23:21 +0530

    Added file which downloads all dependencies

---
+++ src/main/sentitude/machine_learning/ann/setup.sh
@@ -0,0 +1,8 @@
+# This is to install required dependancies
+pip install numpy
+pip install scipy
+sudo apt-get install swig python-pyfann cmake
+wget http://kaz.dl.sourceforge.net/project/fann/fann/2.2.0/FANN-2.2.0-Source.zip
+unzip FANN-2.2.0-Source.zip
+rm FANN-2.2.0-Source.zip
+
